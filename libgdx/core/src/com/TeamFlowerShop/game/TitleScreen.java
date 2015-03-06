932d0c280cec61fcdad8ca7f554f80a26179af09
tchil_000 <tchil_000@SUPERMONSTER.home>
2015-03-05 21:52:28 -0800

    Added Title Screen background and rough start to class.

---
+++ libgdx/core/src/com/TeamFlowerShop/game/TitleScreen.java
@@ -0,0 +1,32 @@
+package com.TeamFlowerShop.game;
+
+/*import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class TitleScreen
+{
+	SpriteBatch batch;
+	Texture Title;
+	float rotation = 0;
+	float TitleX, TitleY;
+	double universalXPos, universalYPos;
+	
+	batch = new SpriteBatch();
+	
+	Title = new Texture("Title.png");
+	
+public void Update(boolean Continue)
+{
+
+}
+public void Draw(float ux, float uy)
+{	
+	batch.begin();
+	batch.draw(Title, (float) ux, (float) uy, Title.getWidth(), .getHeight());
+	batch.end();
+}
+}*/
