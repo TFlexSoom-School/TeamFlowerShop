b8a330aa2d1a424b80e7b42c982ce6a9909a96a8
chilbert <chilbert@CHILBERT-LAP4.redmond.corp.microsoft.com>
2015-03-27 15:16:25 -0700

    A couple new assets and The Titlescreen class is no longer commented out

--- libgdx/core/src/com/TeamFlowerShop/game/TitleScreen.java
+++ libgdx/core/src/com/TeamFlowerShop/game/TitleScreen.java
@@ -1,32 +1,46 @@
 package com.TeamFlowerShop.game;
 
-/*import java.util.ArrayList;
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.*;
 
-public class TitleScreen
-{
+public class TitleScreen {
+
 	SpriteBatch batch;
-	Texture Title;
-	float rotation = 0;
-	float TitleX, TitleY;
-	double universalXPos, universalYPos;
 	
-	batch = new SpriteBatch();
+	Texture titleBackdrop;
+	Texture startButton;
 	
-	Title = new Texture("Title.png");
+	int posX, posY;
 	
-public void Update(boolean Continue)
-{
-
-}
-public void Draw(float ux, float uy)
-{	
-	batch.begin();
-	batch.draw(Title, (float) ux, (float) uy, Title.getWidth(), .getHeight());
-	batch.end();
-}
-}*/
+	// Initialization for the wall
+	public TitleScreen ()
+	{
+		titleBackdrop = new Texture("Title.png");
+		startButton = new Texture("StartA.png");
+		
+		batch = new SpriteBatch();
+		
+	
+	}
+	
+	// Update function for the wall
+	public void Update ()
+	{
+		
+	}
+	
+	// Draw function for the wall
+	public void Draw (float ux, float uy)
+	{
+		batch.begin();
+		
+		batch.draw(titleBackdrop, ux , uy);
+		batch.draw(startButton, ux,  uy);
+		
+		batch.end();
+		
+	}
+	}
+	
