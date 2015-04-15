5eb8b1e2dd84b19f925debf948e0d85966d62267
tchil_000 <tchil_000@SUPERMONSTER.home>
2015-04-14 23:26:31 -0700

    Welp I changed the position of the button. I may choose later to make the fractions 20/64 and 20/48 variables for cleaner code however the mouse still clicks on the start button. In the future I hope to add a start button animation and the illuminati music vis suggested.

--- libgdx/core/src/com/TeamFlowerShop/game/TitleScreen.java
+++ libgdx/core/src/com/TeamFlowerShop/game/TitleScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.*;
+import java.util.ArrayList;
 
 public class TitleScreen {
 
@@ -23,20 +24,30 @@ public class TitleScreen {
 		posY = UniversalYPos;
 		titleBackdrop = new Texture("Title.png");
 		startButton = new Texture("StartA.png");
-		startButtonRect = new Rectangle(posX, posY, startButton.getWidth(), startButton.getHeight());
-
+		startButtonRect = new Rectangle((posX/64)*20, (posY/48)*20, startButton.getWidth(), startButton.getHeight());
+		
 		batch = new SpriteBatch();	
 	}
 	
 
 	public boolean Update()
 	{
+		//startButton = new Texture();
 		MousePosY = Gdx.graphics.getHeight() - Gdx.input.getY();
-		if(startButtonRect.contains(Gdx.input.getX(), MousePosY) && Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+		/*if (startButtonRect.contains(Gdx.input.getX(), MousePosY))
+		{
+			startButton = new Texture("StartB.png");
+		}
+		else
+		{
+			start button = new Texture("StartA.png");
+		}*/
+		if(startButtonRect.contains((Gdx.input.getX()/64)*20, (MousePosY/48)*20) && Gdx.input.isButtonPressed(Input.Buttons.LEFT))
 		{
 			return false;
 		}
 		return true;  //Start has not been clicked
+		
 	}
 	
 
@@ -45,7 +56,7 @@ public class TitleScreen {
 		batch.begin();
 		
 		batch.draw(titleBackdrop, ux , uy);
-		batch.draw(startButton, ux,  uy);
+		batch.draw(startButton, ((Gdx.graphics.getWidth()/64)*20)-ux,  ((Gdx.graphics.getHeight()/48)*20)-uy);
 		
 		batch.end();
 		
